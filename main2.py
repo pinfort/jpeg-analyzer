4c915c059ff6aa72543cabd7b1f889f354079874
pinfort <ptg@nijitei.com>
2018-12-18 14:54:15 +0900

    テスト用mainファイル追加

---
+++ main2.py
@@ -0,0 +1,12 @@
+from segmentsFromFile import SegmentFromFile
+from analyzeSegment import analyzeSegment
+
+def main(path):
+    segs = SegmentFromFile(path)
+    for seg in segs.tokenizeLoop():
+        analyzed = analyzeSegment().analyze(seg["marker"], seg["body"])
+        print(analyzed) if analyzed is not None else ""
+
+if __name__ == '__main__':
+    path = "C:\\Users\\pinfo\\Downloads\\DsmAkbxVYAEKktJ_orig.jpg"
+    main(path)
