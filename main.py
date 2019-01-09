5e68a1125d8626f34fdac2cf094cef5a2a8a1b58
pinfort <ptg@nijitei.com>
2019-01-09 13:37:15 +0900

    メイン関数を書きかえる

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from segmentsFromFile import SegmentFromFile
 from analyzeSegment import analyzeSegment
 
-def main(path):
+def getAnalyzedSegments(path):
+    analyzedSegments = []
+    SOSs = []
     analyzer = analyzeSegment()
     segs = SegmentFromFile(path)
     for seg in segs.tokenizeLoop():
         analyzed = analyzer.analyze(seg["marker"], seg["body"])
-        print(analyzed) if analyzed is not None else ""
+        if analyzed is None:
+            continue
+        if analyzed["segmentName"] == "SOF":
+            print("X:", analyzed["x_size"], "Y:", analyzed["y_size"])
+            for nf in analyzed["nfData"]:
+                print(nf["id"], nf["name"], nf["hn"], "x", nf["vn"])
+        elif analyzed["segmentName"] == "SOS":
+            SOSs.append(analyzed)
+        analyzedSegments.append(analyzed)
+    assert(len(SOSs) == len(segs.f.IMAGES))
+    count = 0
+    for segments in analyzedSegments:
+        if segments["segmentName"] == "SOS":
+            segments["image"] = segs.f.IMAGES[count]
+            count = count + 1
+        yield segments
+
+def main(path):
+    for segment in getAnalyzedSegments(path):
+        print(segment["segmentName"])
 
 if __name__ == '__main__':
     path = "C:\\Users\\pinfo\\Downloads\\DsmAkbxVYAEKktJ_orig.jpg"
